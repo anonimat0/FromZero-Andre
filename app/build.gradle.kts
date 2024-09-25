ec651b999b2300b8a578567a2c9e6c6e4e80c3ef
anonimat0 <u202114714@upc.edu.pe>
2024-09-25 15:38:59 -0500

    esto es un testeo, se eliminara pronto

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.fromzero_andre"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.fromzero_andre"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation ("com.github.qamarelsafadi:CurvedBottomNavigation:0.1.3")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("script-runtime"))
+}
---
+++ app/src/main/java/com/example/fromzero_andre/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.fromzero_andre
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.fromzero_andre.fragments.HomeFragment
+import com.example.fromzero_andre.fragments.MenuFragment
+import com.example.fromzero_andre.fragments.MessageFragment
+import com.example.fromzero_andre.fragments.SearchFragment
+import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bottomNavigation = findViewById<CurvedBottomNavigation>(R.id.bottomNavigation)
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(1, "Home", R.drawable.ic_home)
+        )
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(2, "Search", R.drawable.ic_search)
+        )
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(3, "Message", R.drawable.ic_message)
+        )
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(4, "Menu", R.drawable.ic_menu)
+        )
+
+        bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                1 -> {replaceFragment(HomeFragment())}
+                2 -> {replaceFragment(SearchFragment())}
+                3 -> {replaceFragment(MessageFragment())}
+                4 -> {replaceFragment(MenuFragment())}
+            }
+        }
+
+        //Default Bottom Tab Selected
+        replaceFragment(HomeFragment())
+        bottomNavigation.show(1)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
